688df516de087933fdf7543b20bd387df90ac5c0
Gjokaj, Kevin {HBGD~ROTKREUZ-TRO} <gjokajk@emea.roche.com>
2025-11-21 20:28:27 +0100

    Finish

--- Task2/index.js
+++ Task2/index.js
@@ -1,9 +1,4 @@
-// index.js
 export const sum = (a, b) => {
   return a + b
 }
-
-const unusedVariable = 10 // Added to trigger 'unused-vars' error
-
 console.log(sum(1, 2))
-console.log(myUndefinedVariable) // Triggers 'no-undef'
--- Task2/index.test.js
+++ Task2/index.test.js
@@ -1,14 +1,14 @@
 // index.test.js
-import assert from 'assert';
-import { sum } from './index.js';
+import assert from 'assert'
+import { sum } from './index.js'
 
-describe("sum", () => {
-  it("should add 1 + 2 to equal 3", () => {
-    assert.equal(sum(1, 2), 3);
-  });
+describe('sum', () => {
+  it('should add 1 + 2 to equal 3', () => {
+    assert.equal(sum(1, 2), 3)
+  })
 
   // Additional test for decimals
-  it("should add 1.5 + 2.5 to equal 4", () => {
-    assert.equal(sum(1.5, 2.5), 4);
-  });
-});
+  it('should add 1.5 + 2.5 to equal 4', () => {
+    assert.equal(sum(1.5, 2.5), 4)
+  })
+})
